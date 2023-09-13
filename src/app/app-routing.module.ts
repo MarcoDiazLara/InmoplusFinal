67c9b8f243149d99f1d31ff604fcb0238d801080
MarcoDiazLara <marco.diazl@alumno.buap.mx>
2023-09-13 13:45:16 -0600

    agregue comentario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,6 @@
+
+//prueba dx
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
