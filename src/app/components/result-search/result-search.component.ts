fb8a7bec7d68273118a1815dad4da2699efe861b
IIITunersIII <sanchezmota09@gmail.com>
2023-09-13 16:36:31 -0600

    Result_Search
    cree el componente para empezar a diseñar la pantalla de los resultados de la busqueda.

---
+++ src/app/components/result-search/result-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-result-search',
+  templateUrl: './result-search.component.html',
+  styleUrls: ['./result-search.component.css']
+})
+export class ResultSearchComponent {
+
+}
