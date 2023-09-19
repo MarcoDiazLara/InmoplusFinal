0a4f2d6407e853517bfc0996496cf915d03ed79a
TavoGR9 <guzmanroldangustavo@gmail.com>
2023-09-19 11:45:27 -0600

    se modifico el menu de la barra lateral

--- src/app/components/barralateralmenu/barralateralmenu.component.ts
+++ src/app/components/barralateralmenu/barralateralmenu.component.ts
@@ -18,4 +18,16 @@ export class BarralateralmenuComponent {
     }
     }
 }
+
+  toggleSubmenu2() {
+    const submenuContainer2 = document.getElementById('submenu_config2');
+
+    if(submenuContainer2) { 
+      if (submenuContainer2.style.display === 'none' || submenuContainer2.style.display === '') {
+        submenuContainer2.style.display = 'block'; // Mostrar el submenu
+    } else {
+        submenuContainer2.style.display = 'none'; // Ocultar el submenu
+    }
+    }
+  }
 }
