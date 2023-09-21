a6f08e43e2afe20a6ffe7ee812acfd62cedd07bf
Alejandra2050 <75100495+Alejandra2050@users.noreply.github.com>
2023-09-20 21:00:02 -0600

    menu actualizado y logo y carrusel falta

--- src/app/components/carrusel/carrusel.component.ts
+++ src/app/components/carrusel/carrusel.component.ts
@@ -1,42 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-carrusel',
   templateUrl: './carrusel.component.html',
   styleUrls: ['./carrusel.component.css']
 })
-export class CarruselComponent {
-
-  imgCollection: Array<object> = [
-    {
-       
-      image: 'assets/6.jpg',
-      thumbImage: 'assets/6.jpg',
-      alt: 'Image 1',
-      
-    }, {
-      image: 'assets/8.jpg',
-      thumbImage: 'assets/8.jpg',
-      
-      alt: 'Image 2'
-    },
-  ];
+export class CarruselComponent implements OnInit{
 
 
+  images = [
+    { src: 'imagen1.jpg', alt: 'Imagen 1' },
+    { src: 'imagen2.jpg', alt: 'Imagen 2' }
+  ];
 
+  currentIndex = 0;
+  interval: any;
 
+  constructor() { }
 
+  ngOnInit(): void {
+    this.startCarousel();
+  }
 
+  startCarousel() {
+    this.interval = setInterval(() => {
+      this.currentIndex = (this.currentIndex + 1) % this.images.length;
+    }, 1000); // Cambia de imagen cada 3 segundos
+  }
 
-
-
-
-
-
-
-
-
-
-
-
+  ngOnDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+  }
+  
+ 
 }
+
+ 
+
+  
+
+  
+   
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
